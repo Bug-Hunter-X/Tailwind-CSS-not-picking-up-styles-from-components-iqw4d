37550643fa1496d10cc6c386e4bb3025b8a66075
BugHunter-X <askzenai@gmail.com>
2024-12-16 15:26:09 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,11 @@
+```javascript
+// ... your Tailwind CSS configuration
+module.exports = {
+  // ... other configurations
+  content: ['./src/**/*.{html,js,jsx,ts,tsx,vue}'], // Added .vue and double-checked the path
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+```
